51d9cc7b19952a67db17c3d9c7b572c156e12847
Unknown <paul.remignon@gmail.com>
2021-02-06 22:48:51 +0100

    esp-song: wifi connection with audio status

--- esp-song/esp-song.ino
+++ esp-song/esp-song.ino
@@ -6,30 +6,52 @@
  * 
  * 
  */
+#include <ESP8266WiFi.h>
+#include "wificonf.h" // contains wifiNetwork wifiPassword
 
-int notes[] = {523, 587, 659, 698, 784, 880, 987, 1046, 1108};
+int NOTES[] = {523, 587, 659, 698, 784, 880, 987, 1046, 1108};
+WiFiServer server(80);
 
 int testPin = D1;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(testPin, OUTPUT);
+
+  // wifi startup
+  Serial.begin(115200);
+  delay(10);
+  // Connect to WiFi network
+  Serial.print("Connecting to ");
+  Serial.println(wifiNetwork);
+  WiFi.begin(wifiNetwork, wifiPassword);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(400);
+    Serial.print(".");
+    espTone(testPin,NOTES[0],100);
+  }
+  Serial.println("");
+  Serial.printf("WiFi connected,local IP %s in a web browser\n", WiFi.localIP().toString().c_str());
+  server.begin();
+  Serial.printf("Server started");
+  espTone(testPin,NOTES[7],500);
+  espTone(testPin,NOTES[0],500);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  musicalScale(D1);
+  //musicalScale(D1);
 
 }
 
 // simple gamme
 void musicalScale(int pin){
   for (int i=0;i<8;i++){
-    espTone(pin,notes[i],500);
+    espTone(pin,NOTES[i],500);
   }
 }
 
-// base method for simle sound
+// base method for simple sound
 void espTone(uint8_t _pin, unsigned int frequency, unsigned long duration) {
   pinMode (_pin, OUTPUT );
   analogWriteFreq(frequency);
